5fee4ccc45dccc502409b0f05c3c19543f71d585
Darryl <109982526+DarrylJulyansyah@users.noreply.github.com>
2022-08-07 21:11:59 +0700

    Add files via upload

---
+++ project/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SISTEM PARKIR SMKS IGASAR PINDAD</title>
+	<frameset rows="15%, *, 10%">
+		<frame src="profil.php"></frame>
+	<frameset cols="30%, *">
+		<frame src="home.php"></frame>
+		<frame src="tengah.html" name="tengah"></frame>
+	</frameset>
+		<frame src="bawah.html"></frame>
+	</frameset>
+</head>
+<body>
+
+</body>
+</html>
